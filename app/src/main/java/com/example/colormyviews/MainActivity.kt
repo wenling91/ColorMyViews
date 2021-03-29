57171a7fb2ddabd9865a89a38da26b2ae6a0dc69
NDHU_CSIE <ndhu_csie@PC-34.local>
2021-03-29 15:26:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.colormyviews
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.colormyviews.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(
+            binding.boxOne, binding.boxTwo, binding.boxThree,
+            binding.boxFour, binding.boxFive, binding.constraintLayout
+            )
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for the background
+            R.id.box_one -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five -> view.setBackgroundColor(Color.BLUE)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
